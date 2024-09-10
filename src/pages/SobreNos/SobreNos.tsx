2174fb5d74a9a59f6d6cd7d13c2563beeee23c30
MarcellaRGSanches <rochaamarcellaaa@gmail.com>
2024-09-10 18:18:30 -0300

    Passado o SobreNos da branch 8 para a 10 Arrumado a questão das fotos E alterado svg(index.html,branch 8 também)

--- src/pages/SobreNos/SobreNos.tsx
+++ src/pages/SobreNos/SobreNos.tsx
@@ -1,7 +1,129 @@
+import { LinkedinLogo } from "@phosphor-icons/react"
+import { GithubLogo } from "@phosphor-icons/react/dist/ssr"
 
 function SobreNos() {
   return (
-    <div>SobreNos</div>
+
+    <div>
+      <div className="  flex justify-center font-italiana text-reuse-green h-30 px-10 ">
+        <div className=" space-y-2  container flex flex-col items-center py-9">
+          <p className='text-4xl font-bold'>Bem-vindo à Reuse Moda Sustentável! </p>
+          <p className='py-8 text-2xl font-sans'>Somos um grupo de pessoas apaixonadas pela moda e pelo meio ambiente.
+            Nossa missão é transformar o conceito de consumo de moda,
+            promovendo escolhas mais conscientes e sustentáveis.
+            Acreditamos que cada peça de roupa merece uma segunda chance,
+            e por isso criamos a ReUse Moda Sustentável,
+            um e-commerce dedicado à venda de roupas de brechó com curadoria cuidadosa.
+          </p>
+
+          <div className=" space-y-2  container flex flex-col items-center ">
+            <p className='text-4xl font-bold'>Nossa Missão! </p>
+            <p className='py-8 text-2xl font-sans'>Nosso objetivo é minimizar o impacto ambiental da indústria da moda,
+              promovendo o reuso e a reinvenção de peças que ainda têm muito a oferecer.
+              Cada roupa disponível na ReUse Moda Sustentável foi cuidadosamente selecionada,
+              valorizando sua qualidade e seu potencial de continuar a proporcionar estilo e propósito
+            </p>
+            <p className='text-4xl font-bold'>Equipe </p>
+
+            <div className="flex flex-wrap justify-center gap-4">
+
+
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="flex justify-center font-bold text-xl mb-1">Amanda</div>
+                  <img src="https://ik.imagekit.io/sfl8kzqbl/Amanda%20apresenta%C3%A7%C3%A3o%20.png?updatedAt=1725980487931" alt="Descrição da imagem" className="w-full h-auto"></img>
+                  <p className="text-black flex justify-center font-mono text-2xl">
+                    <a className=" flex justify-center " href="https://github.com/amandammgr" target="_blank">
+                      <GithubLogo size={32} weight="fill" /></a>
+                    <a className=" flex justify-center " href="https://www.linkedin.com/in/amandamachadomgr/" target="_blank">
+                      <LinkedinLogo size={32} weight="fill" /></a>
+                  </p>
+                </div>
+              </div>
+
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="flex justify-center font-bold text-xl mb-1">Brunna.</div>
+                  <img src="https://ik.imagekit.io/sfl8kzqbl/Brunna%20Apresentacao.png?updatedAt=1725980487672" alt="Descrição da imagem" className="w-full h-auto"></img>
+                  <p className="text-black flex justify-center font-mono text-2xl">
+                    <a className=" flex justify-center " href="https://github.com/Brunna-Bonilha" target="_blank">
+                      <GithubLogo size={32} weight="fill" /></a>
+                    <a className=" flex justify-center " href="https://www.linkedin.com/in/bbonilha/" target="_blank">
+                      <LinkedinLogo size={32} weight="fill" /></a>
+                  </p>
+                </div>
+              </div>
+
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="flex justify-center font-bold text-xl mb-1">Everton P.</div>
+                  <img src="https://ik.imagekit.io/sfl8kzqbl/Everton%20apresentacao%20.png?updatedAt=1725980487698" alt="Descrição da imagem" className="w-full h-auto"></img>
+                  <p className="text-black flex justify-center font-mono text-2xl">
+                    <a className=" flex justify-center " href="https://github.com/Evertonpsf" target="_blank">
+                      <GithubLogo size={32} weight="fill" /></a>
+                    <a className=" flex justify-center " href="https://www.linkedin.com/in/everton-figueiredo-3722b5178/" target="_blank">
+                      <LinkedinLogo size={32} weight="fill" /></a>
+                  </p>
+                </div>
+              </div>
+
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="flex justify-center font-bold text-xl mb-1">Jhonatan.</div>
+                  <img src="https://ik.imagekit.io/sfl8kzqbl/Jhonata%20apresentacao%20.png?updatedAt=1725980487592" alt="Descrição da imagem" className="w-full h-auto"></img>
+                  <p className="text-black flex justify-center font-mono text-2xl">
+                    <a className=" flex justify-center " href="https://github.com/Jhonata-vss" target="_blank">
+                      <GithubLogo size={32} weight="fill" /></a>
+                    <a className=" flex justify-center " href="https://www.linkedin.com/in/jhonata-venicius/" target="_blank">
+                      <LinkedinLogo size={32} weight="fill" /></a>
+                  </p>
+                </div>
+              </div>
+
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="flex justify-center font-bold text-xl mb-1">Marcella.</div>
+                  <img src="https://ik.imagekit.io/sfl8kzqbl/Marcella%20apresentacao.png?updatedAt=1725980487934" alt="Descrição da imagem" className="w-full h-auto"></img>
+                  <p className="text-black flex justify-center font-mono text-2xl">
+                    <a className=" flex justify-center " href="https://github.com/ReUse-Moda-Sustentavel" target="_blank">
+                      <GithubLogo size={32} weight="fill" /></a>
+                    <a className=" flex justify-center " href="https://github.com/ReUse-Moda-Sustentavel" target="_blank">
+                      <LinkedinLogo size={32} weight="fill" /></a>
+                  </p>
+                </div>
+              </div>
+
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="flex justify-center font-bold text-xl mb-1">Maria.</div>
+                  <img src="https://ik.imagekit.io/sfl8kzqbl/Maria%20apresentacao%20(8).png?updatedAt=1725980487879" alt="Descrição da imagem" className="w-full h-auto"></img>
+                  <p className="text-black flex justify-center font-mono text-2xl">
+                    <a className=" flex justify-center " href="https://github.com/mpaulas" target="_blank">
+                      <GithubLogo size={32} weight="fill" /></a>
+                    <a className=" flex justify-center " href="https://www.linkedin.com/in/mpaulas/" target="_blank">
+                      <LinkedinLogo size={32} weight="fill" /></a>
+                  </p>
+                </div>
+              </div>
+
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="flex justify-center font-bold text-xl mb-1">Vinicius.</div>
+                  <img src="https://ik.imagekit.io/sfl8kzqbl/Vinicius%20apresentacao.png?updatedAt=1725980487750" alt="Descrição da imagem" className="w-full h-auto"></img>
+                  <p className="text-black flex justify-center font-mono text-2xl">
+                    <a className=" flex justify-center " href="https://github.com/vinicius-zanchetta" target="_blank">
+                      <GithubLogo size={32} weight="fill" /></a>
+                    <a className=" flex justify-center " href="https://www.linkedin.com/in/vinicius-zanchetta/" target="_blank">
+                      <LinkedinLogo size={32} weight="fill" /></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
